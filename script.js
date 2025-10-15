482272f3c134317817dfbe3d884ee09013fad268
BBartosz10 <kamlbaron3523@gmail.com>
2025-10-15 16:24:48 +0200

    ZMiana js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 function odliczanie() {
-            let Data = new Date();
+    let Data = new Date();
 
-            let dzien = Data.getDate();
-            let miesiac = Data.getMonth()+1;
-            let rok = Data.getFullYear();
+    let dzien = Data.getDate();
+    let miesiac = Data.getMonth()+1;
+    let rok = Data.getFullYear();
 
-            let godzina = Data.getHours();
-            if(godzina<10) godzina = "0"+godzina;
-            let minuta = Data.getMinutes();
-            if(minuta<10) minuta = "0"+minuta;
-            let sekunda = Data.getSeconds();
-            if(sekunda<10) sekunda = "0"+sekunda;
+    let godzina = Data.getHours();
+    if(godzina<10) godzina = "0"+godzina;
+    let minuta = Data.getMinutes();
+    if(minuta<10) minuta = "0"+minuta;
+    let sekunda = Data.getSeconds();
+    if(sekunda<10) sekunda = "0"+sekunda;
 
-            document.getElementById("zegar").innerHTML = dzien+"/"+miesiac+"/"+rok+"|"+godzina+":"+minuta+":"+sekunda;
+    document.getElementById("zegar").innerHTML = dzien+"/"+miesiac+"/"+rok+"|"+godzina+":"+minuta+":"+sekunda;
 
-            setTimeout("odliczanie()", 1000)
-        }
+    setTimeout("odliczanie()", 1000)
+}
